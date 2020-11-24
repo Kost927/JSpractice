62cbb71fb4b7b4206640c3cb4ecf0cbcd0bf87b8
Kostya Polishko <kost927@gmail.com>
2020-11-24 20:23:38 +0200

    init

--- src/modal.js
+++ src/modal.js
@@ -32,34 +32,4 @@ class ModalPopUp {
   }
 }
 
-const container = document.querySelector(".modal");
-
-const modalModule = component => {
-  const closeModalComponent = () => {
-    container.innerHTML = "";
-    document.body.style.overflow = "auto";
-  };
-
-  const closeModalWindow = e => {
-    if (e.target.classList.contains("modalOverlay") || e.target.dataset.action === "btn__closeModal" || e.key === "Escape") {
-      container.innerHTML = "";
-      document.body.style.overflow = "auto";
-    }
-  };
-
-  const markup = `
-    <div class="modalOverlay">
-    <div class="modalComponent"></div>
-  </div>
-    `;
-
-  container.innerHTML = markup;
-
-  const modalComponent = document.querySelector(".modalComponent");
-  modalComponent.innerHTML = component();
-  const modalOverlay = document.querySelector(".modalOverlay");
-  modalOverlay.addEventListener("click", closeModalWindow);
-  window.addEventListener("keydown", closeModalWindow);
-};
-
 export { ModalPopUp };
