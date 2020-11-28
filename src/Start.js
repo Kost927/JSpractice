83f4699bc0f4b1a8be94db386ae3966d018e5e98
Kostya Polishko <kost927@gmail.com>
2020-11-28 14:25:24 +0200

    need

--- src/Start.js
+++
@@ -1,9 +0,0 @@
-import TaskList from "./TasksList.js";
-import Popup from "./Popup.js";
-
-
-window.addEventListener("load", () => {
-  new TaskList().showTasks();
-  new TaskList().setupListeners();
-  new Popup().setupListeners();
-});
